be5c39834ab28ab7dfc8d65915f17ab951be3a9c
Vlad Ursu <ursu.vlad94@yahoo.com>
2016-07-08 22:15:00 +0300

    Testing GitHub compatibility

---
+++ whispers/src/whispers/HelloWorld.java
@@ -0,0 +1,11 @@
+package whispers;
+
+// Class purely made for testing github compatibility
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub");
+	}
+
+}
